ce61e4a6cae902f28de7a09993485560f8fd946d
Pete Bachant <petebachant@gmail.com>
2015-11-04 11:51:13 -0500

    Plot angle of attack

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-This script plots results from the turbinesFoam actuator line tutorial.
+This script plots results from `paramsweep.py`.
 """
 from __future__ import division, print_function
 import matplotlib.pyplot as plt
@@ -9,36 +9,18 @@ import numpy as np
 import os
 import seaborn as sns
 
-H = 1.0
 U_infty = 1.0
 
-def plot_spanwise():
-    """
-    Plot spanwise distribution of angle of attack and relative velocity.
-    """
-    elements_dir = "postProcessing/actuatorLineElements/0"
-    elements = os.listdir(elements_dir)
-    dfs = {}
-    z_H = np.zeros(len(elements))
-    urel = np.zeros(len(elements))
-    alpha_deg = np.zeros(len(elements))
-    for e in elements:
-        i = int(e.replace("foilElement", "").replace(".csv", ""))
-        df = pd.read_csv(os.path.join(elements_dir, e))
-        z_H[i] = df.z.iloc[-1]/H
-        urel[i] = df.rel_vel_mag.iloc[-1]/U_infty
-        alpha_deg[i] = df.alpha_deg.iloc[-1]
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7.5, 3.25))
-    ax[0].plot(z_H, alpha_deg)
-    ax[0].set_ylabel(r"$\alpha$ (deg)")
-    ax[1].plot(z_H, urel)
-    ax[1].set_ylabel(r"$ | U_{\mathrm{rel}} | / U_\infty $")
-    for a in ax:
-        a.set_xlabel("$z/H$")
-        a.grid(True)
-    fig.tight_layout()
-    
+
 if __name__ == "__main__":
-    sns.set(style="white", context="paper", font_scale=1.5)
-    plot_spanwise()
+    sns.set(style="white", context="paper", font_scale=1.5,
+            rc={"axes.grid": True, "legend.frameon": True})
+    df = pd.read_csv("processed/alpha_sweep.csv")
+    fig, ax = plt.subplots()
+    ax.plot(df.alpha_geom_deg, df.alpha_deg, "o", label="Detected")
+    ax.plot(df.alpha_geom_deg, df.alpha_geom_deg, "--", label="Geometric")
+    ax.set_xlabel(r"$\alpha$ (geometric, degrees)")
+    ax.set_ylabel(r"$\alpha$ (detected, degrees)")
+    ax.legend(loc="lower right")
+    fig.tight_layout()
     plt.show()
